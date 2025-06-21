e5809583b86a0d10600898dd63fa46242890234b
tupe-atharva <atharva.tupe007@gmail.com>
2025-06-21 11:21:10 +0530

    Creation of array - static and dynamic.

---
+++ JavaScript Learning/arrays.js
@@ -0,0 +1,10 @@
+// Static Creation :
+let arr = [1,2,3,4]; // same type of elements.
+let arr1 = [1 , 'atharva' , true]; // different type of elements.
+console.log(arr);
+console.log(arr1);
+console.log(typeof(arr)); // array type is object
+
+// Dynamic Creation - using Array() constructor.
+let arr2 = new Array(1,2,3);
+console.log(arr2);
